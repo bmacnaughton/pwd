ba72d634ca63930bc3f5222f74fda2ecdb1981c3
bmacnaughton <bmacnaughton@gmail.com>
2024-02-08 12:47:49 -0800

    better async version

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,92 @@
+use futures::channel::mpsc::{self};
+use futures::executor::{self, ThreadPool, ThreadPoolBuilder};
+use futures::stream::StreamExt;
 use rand::{rngs::OsRng, RngCore};
-use std::{sync::mpsc, thread, time};
+use std::time::Instant;
 
-//static mut RNG: OsRng = OsRng {};
+//use std::mpsc::{UnboundedReceiver, UnboundedSender};
+use std::time;
 
-struct PasswordImprover {
-    ch: mpsc::SyncSender<String>,
+pub struct PwdImprover {
+    threads: ThreadPool,
+    rng: OsRng,
 }
 
-impl PasswordImprover {
-    fn new(ch: mpsc::SyncSender<String>) -> Self {
-        Self { ch }
+impl PwdImprover {
+    pub fn new(n: usize) -> Self {
+        let threads = ThreadPoolBuilder::new().pool_size(n).create().unwrap();
+        let rng = OsRng {};
+        PwdImprover { threads, rng }
     }
 
-    // password - the user supplied password
-    // length - the length of the bytes to add to the password
-    fn improve(&self, password: &str, length: usize) {
-        const MIN_SLEEP: u64 = 1;
-        const MAX_SLEEP: u64 = 3;
-        let mut range: OsRng = OsRng {};
+    pub async fn improve(&self, password: &str, additional_length: usize) -> String {
+        let mut range = self.rng;
+        let (tx, mut rx) = mpsc::unbounded::<String>();
 
-        let char_set = format!("0123456789!&#_@*~$={}", password);
-        let mut result: Vec<u8> = vec![0; length + password.len()];
-        // where to insert the user-supplied password into the result
+        let char_set: Vec<char> = format!("0123456789!&#_@*~$={}", password).chars().collect();
+
+        let mut result: Vec<char> = vec!['0'; additional_length + password.len()];
         let insert_index = range.next_u32() as usize % (char_set.len() - password.len());
 
-        // this is technically not correct as you just can't treat chars as bytes. either
-        // result needs to be Vec<char> or password and char_set need to be Vec<u8> (or &[u8]).
         for elem in result.iter_mut().take(insert_index) {
-            *elem = char_set.as_bytes()[range.next_u32() as usize % char_set.len()];
+            *elem = char_set[range.next_u32() as usize % char_set.len()];
         }
-
-        for (ix, elem) in result
-            .iter_mut()
-            .skip(insert_index)
-            .take(password.len())
-            .enumerate()
-        {
-            *elem = password.as_bytes()[ix];
+        let mut pwd_iter = password.chars();
+        for elem in result.iter_mut().skip(insert_index).take(password.len()) {
+            *elem = pwd_iter.next().unwrap();
         }
-
         for elem in result.iter_mut().skip(insert_index + password.len()) {
-            *elem = char_set.as_bytes()[range.next_u32() as usize % char_set.len()];
+            *elem = char_set[range.next_u32() as usize % char_set.len()];
         }
 
-        let ch = self.ch.clone();
-
-        // pretend this is doing some work or syscalls or something
-        thread::spawn(move || {
-            let sleep = time::Duration::from_secs(
-                rand::random::<u64>() % (MAX_SLEEP - MIN_SLEEP) + MIN_SLEEP,
-            );
+        let fut_tx = async move {
+            let sleep = time::Duration::from_secs(rand::random::<u64>() % 3 + 1);
             std::thread::sleep(sleep);
+            tx.unbounded_send(result.iter().collect())
+                .expect("send failed");
+        };
 
-            ch.send(String::from_utf8_lossy(&result).to_string())
-                .unwrap();
-        });
+        self.threads.spawn_ok(fut_tx);
+
+        rx.next().await.unwrap()
     }
 }
 
 fn main() {
-    test_password_improver()
+    let improver = PwdImprover::new(4);
+    let now = Instant::now();
+    let improved = executor::block_on(improver.improve("hello", 32));
+    println!("Elapsed={:?} for {}", now.elapsed().as_millis(), improved);
 }
 
-fn test_password_improver() {
-    let (tx, rx) = mpsc::sync_channel(1);
-    let improver = PasswordImprover { ch: tx };
+#[cfg(test)]
+mod test {
+    use super::*;
+    use futures::future::join_all;
 
-    let bad_passwords = vec![
-        "hello".to_string(),
-        "badPassword".to_string(),
-        "veryShort".to_string(),
-    ];
-
-    for password in bad_passwords {
-        improver.improve(&password, 32);
-        match rx.recv() {
-            Ok(pw) => {
-                if !pw.contains(&password) {
-                    panic!("{} does not contain {}", pw, password)
-                } else {
-                    println!("{} improved to {}", password, pw);
-                };
-            }
-            Err(_) => return,
+    #[test]
+    fn test_password_improver() {
+        let bad_passwords = vec![
+            "hello".to_string(),
+            "badPassword".to_string(),
+            "veryShort".to_string(),
+            "my name".to_string(),
+            "my cat's name".to_string(),
+        ];
+        let improver = PwdImprover::new(4);
+        let mut futures = vec![];
+        let now = Instant::now();
+        for password in bad_passwords.iter() {
+            futures.push(improver.improve(password, 32));
+        }
+        let results = executor::block_on(join_all(futures));
+        println!("Elapsed={:?}", now.elapsed());
+        for (password, result) in bad_passwords.iter().zip(results.iter()) {
+            if !result.contains(password) {
+                panic!("{} does not contain {}", result, password)
+            } else {
+                println!("{} improved to {}", password, result);
+            };
         }
     }
 }
